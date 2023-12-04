15d8fe24a0ec6ad24c7ad6cdb4af86839ba24960
yashashrinimje <86704675+yashashrinimje@users.noreply.github.com>
2023-12-04 12:11:22 +0530

    update

--- solid_principles/ISP.py
+++ solid_principles/ISP.py
@@ -1,20 +1,20 @@
 #With ISP
-from abc import ABC, abstractmethod
-class Workable(ABC):
+from abc import ABC, abstractmethod 
+class Workable(ABC):    #workable interface contains a single work method repesenting the ability to work .
     @abstractmethod
     def work(self):
         pass
 
-class Eatable(ABC):
+class Eatable(ABC):      #Eatable interface contains a single eat method repesenting the ability to eat
     @abstractmethod
     def eat(self):
         pass
 
-class Worker(Workable, Eatable):
-    def work(self):
+class Worker(Workable, Eatable):       # worker class implements both workable and Eatable interfaces.
+    def work(self):         # implementation of work class
         print("Worker is working.")
 
-    def eat(self):
+    def eat(self):          # implemantation of eat class
         print("Worker is eating.")
         
 w1 = Worker() 
